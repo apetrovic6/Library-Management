0ddb578a501bc744b25cf73d86f54b333f88269d
apetrovic6 <petrovicante6@gmail.com>
2023-01-26 17:39:06 +0100

    Init Book model

---
+++ Books/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Books.Models;
+
+public class Book
+{
+    [Key]
+    public int Id { get; set; }
+    
+    [Required]
+    public string Name { get; set; }
+}
