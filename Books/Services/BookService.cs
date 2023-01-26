f1aedc8f3c372739a37404af36f8ea19262cd7a8
apetrovic6 <petrovicante6@gmail.com>
2023-01-26 18:16:33 +0100

    Initial implementation of book service

---
+++ Books/Services/BookService.cs
@@ -0,0 +1,29 @@
+﻿using Books.Data;
+using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Books.Services;
+
+public class BookService : Books.BooksBase
+{
+    private readonly BooksDbContext _context;
+
+    public BookService(BooksDbContext context)
+    {
+        _context = context;
+    }
+
+    public override async Task<GetBooksResponse> GetBooks(GetBooksRequest request, ServerCallContext context)
+    {
+        var books = await _context.Books.ToListAsync();
+    
+        var response = new GetBooksResponse();
+    
+        foreach (var book in books)
+        {
+            response.Books.Add(new BookModel() { Id = book.Id, Name = book.Name, Stock = book.Stock});
+        }
+    
+        return response;
+    }
+}
