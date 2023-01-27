f817e817593fbd59aaa8a7cd65414d1fdf1a1055
apetrovic6 <petrovicante6@gmail.com>
2023-01-27 09:44:24 +0100

    Use automapper

--- Books/Services/BookService.cs
+++ Books/Services/BookService.cs
@@ -20,28 +20,17 @@ public class BookService : Books.BooksBase
     public override async Task<GetBooksResponse> GetBooks(GetBooksRequest request, ServerCallContext context)
     {
         var books = await _context.Books.ToListAsync();
-    
-        var response = new GetBooksResponse();
-    
-        foreach (var book in books)
-        {
-            response.Books.Add(new BookModel() { Id = book.Id, Name = book.Name, Stock = book.Stock});
-        }
-    
-        return response;
+        return _mapper.Map<GetBooksResponse>(books);
     }
 
     public override async Task<CreateBookResponse> CreateBook(CreateBookRequest request, ServerCallContext context)
     {
-        var newBook = new Book() { Name = request.Name, Stock = request.Stock };
+        var newBook = _mapper.Map<Book>(request);
 
         await _context.Books.AddAsync(newBook);
-
         await _context.SaveChangesAsync();
-        
-        CreateBookResponse response = new () {Book = new BookModel{ Id = newBook.Id, Name = newBook.Name, Stock = newBook.Stock }};
-        
-        return response;
+
+        return _mapper.Map<CreateBookResponse>(newBook);
     }
 
     public async override Task<UpdateBookResponse> UpdateBook(UpdateBookRequest request, ServerCallContext context)
@@ -57,14 +46,12 @@ public class BookService : Books.BooksBase
     public override async Task<DeleteBookResponse> DeleteBook(DeleteBookRequest request, ServerCallContext context)
     {
         var bookToDelete = await _context.Books.Where(b => b.Id == request.Id).FirstOrDefaultAsync();
-
-        if (bookToDelete == null) return null;
-
-         _context.Books.Remove(bookToDelete);
-         await _context.SaveChangesAsync();
+        
+        if (bookToDelete == null) return  new DeleteBookResponse { Deleted = false, Message = "Not Found"};
+        
+        _context.Books.Remove(bookToDelete);
+        var deleted = (await _context.SaveChangesAsync()) > 0;
          
-         var response = new DeleteBookResponse { Book = new BookModel {Id = bookToDelete.Id, Name = bookToDelete.Name, Stock = bookToDelete.Stock }};
-         
-         return response;
+         return new DeleteBookResponse { Deleted = deleted, Message = $"Deleted Book: {bookToDelete.Name}"};
     }
 }
