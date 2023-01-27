dd63329f15e8742e09e5bffe46eeed752db4437f
apetrovic6 <petrovicante6@gmail.com>
2023-01-27 09:43:39 +0100

    Refactor

--- Gateway/GraphQL/Queries/Query.cs
+++ Gateway/GraphQL/Queries/Query.cs
@@ -11,23 +11,11 @@ public class Query
     {
         _configuration = configuration;
     }
-
-    public async Task<HelloReply> GetHello(string name)
-    {
-        var channel = GrpcChannel.ForAddress("http://books-clusterip-service");
-        var client = new Greeter.GreeterClient(channel);
-        var reply = await client.SayHelloAsync(new HelloRequest{ Name = name});
-        Console.WriteLine(reply);
-        return  reply;
-    }
-
+    
     public async Task<GetBooksResponse> GetBooks()
     {
         var channel = GrpcChannel.ForAddress(_configuration["BooksService"]);
         var client = new Books.Books.BooksClient(channel);
-        var reply = await client.GetBooksAsync(new GetBooksRequest());
-        Console.WriteLine(reply.Books);
-        
-        return reply;
+        return await client.GetBooksAsync(new GetBooksRequest());
     }
 }
