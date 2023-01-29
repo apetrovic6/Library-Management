830caf1450948823aaa218c54a906a3263074f1b
apetrovic6 <petrovicante6@gmail.com>
2023-01-29 20:44:31 +0100

    Setup MudBlazor

--- WebClient/Program.cs
+++ WebClient/Program.cs
@@ -1,12 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using WebClient;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
 builder.Services.AddBooksClient()
     .ConfigureHttpClient(client => client.BaseAddress = new Uri("https://acme.com/graphql"));
 
