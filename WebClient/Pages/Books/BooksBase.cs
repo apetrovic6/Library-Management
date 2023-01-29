9ea8ff51008d624048a5dac461e776d8e5aa3cb6
apetrovic6 <petrovicante6@gmail.com>
2023-01-29 20:46:16 +0100

    Fetch and display books.

---
+++ WebClient/Pages/Books/BooksBase.cs
@@ -0,0 +1,17 @@
+﻿using BooksGQL;
+using Microsoft.AspNetCore.Components;
+
+namespace WebClient.Pages.Books;
+
+public class BooksBase : ComponentBase
+{
+    [Inject] private BooksClient _client { get; set; }
+    protected IGetBooksResult? fetchedBooks { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        var result = await _client.GetBooks.ExecuteAsync();
+
+        fetchedBooks = result.Data;
+    }
+}
