62f30ce525c455e156a08d35c2a8af91f03ae6ce
apetrovic6 <petrovicante6@gmail.com>
2023-01-30 17:28:36 +0100

    Add new props to book graphql type

--- Gateway/GraphQL/Types/BookType.cs
+++ Gateway/GraphQL/Types/BookType.cs
@@ -4,5 +4,10 @@ public class BookType
 {
     public int Id { get; set; }
     public string Title { get; set; }
+    public string Author { get; set; }
+    public int Year { get; set; }
+    public string Country { get; set; }
+    public string Language { get; set; }
+    public string ImageLink { get; set; }
     public int Stock { get; set; }
 }
