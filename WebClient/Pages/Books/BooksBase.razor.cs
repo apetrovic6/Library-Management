51b840aaa18edd283f56fa358e6253ca8edb13df
apetrovic6 <petrovicante6@gmail.com>
2023-01-30 17:31:42 +0100

    Add navigation to book detail page

--- WebClient/Pages/Books/BooksBase.razor.cs
+++ WebClient/Pages/Books/BooksBase.razor.cs
@@ -6,12 +6,18 @@ namespace WebClient.Pages.Books;
 public class BooksBase : ComponentBase
 {
     [Inject] private BooksClient _client { get; set; }
-    protected IGetBooksResult? fetchedBooks { get; set; }
+    [Inject] private NavigationManager Navigation { get; set; }
+    protected IGetBooksResult? FetchedBooks { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
         var result = await _client.GetBooks.ExecuteAsync();
 
-        fetchedBooks = result.Data;
+        FetchedBooks = result.Data;
+    }
+
+    protected void NavigateToBook(int Id)
+    {
+        Navigation.NavigateTo($"book/{Id}");
     }
 }
