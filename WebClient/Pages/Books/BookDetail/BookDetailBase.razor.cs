672495f4ae25eec1d1d46865308738825968d67d
apetrovic6 <petrovicante6@gmail.com>
2023-01-31 17:23:36 +0100

    Initial implementation of BookDetail page

--- WebClient/Pages/Books/BookDetail/BookDetailBase.razor.cs
+++ WebClient/Pages/Books/BookDetail/BookDetailBase.razor.cs
@@ -1,27 +1,46 @@
 ﻿using BooksGQL;
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using StrawberryShake;
+
 namespace WebClient.Pages.Books;
 
 public class BookDetailBase : ComponentBase
 {
     [Inject] private BooksClient client { get; set; }
     [Inject] private NavigationManager Navigation { get; set; }
-    
-    [Parameter]
-    public int BookId { get; set; }
+    [Inject] private ISnackbar Snackbar { get; set; }
+    [Parameter] public int BookId { get; set; }
 
- 
-    protected  IGetBookById_BookById_Book? Book { get; set; }
 
-    protected void GoToEditPage(IGetBookById_BookById_Book? book)
+    protected IGetBookById_BookById_Book? Book { get; set; }
+
+    protected void GoToEditPage()
     {
-        Navigation.NavigateTo($"/book/{book?.Id}/edit");
-    } 
-    
+        Navigation.NavigateTo($"/book/{Book?.Id}/edit");
+    }
+
+    protected async void DeleteBook()
+    {
+        var res = await client.DeleteBook.ExecuteAsync(Book.Id);
+        if (res.IsSuccessResult())
+        {
+            Snackbar.Add($"Book {Book.Title} deleted successfully", Severity.Success);
+            Navigation.NavigateTo("/books");
+        }
+        else
+        {
+            foreach (var err in res.Errors)
+            {
+                Snackbar.Add($"Error: {err.Message}", Severity.Error);
+            }
+        }
+    }
+
     protected override async Task OnInitializedAsync()
     {
-     var res = await client.GetBookById.ExecuteAsync(BookId);
+        var res = await client.GetBookById.ExecuteAsync(BookId);
 
-      Book = res.Data?.BookById.Book;
+        Book = res.Data?.BookById.Book;
     }
 }
