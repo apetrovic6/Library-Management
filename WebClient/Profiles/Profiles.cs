89de80f9281c674dcb05d8c17ed49cbd21ac6c2f
apetrovic6 <petrovicante6@gmail.com>
2023-01-31 17:21:20 +0100

    Add AutoMapper profiles for DTO -> Book Input mapping

---
+++ WebClient/Profiles/Profiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BooksGQL;
+using WebClient.DTO;
+
+namespace WebClient.Profiles;
+
+public class Profiles : Profile
+{
+    public Profiles()
+    {
+        CreateMap<AddBookDto, CreateBookInput>();
+        CreateMap<UpdateBookDto, UpdateBookInput>();
+    }
+}
