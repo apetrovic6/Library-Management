7996d3c813672731a677574b41d789b1c9c26b97
apetrovic6 <petrovicante6@gmail.com>
2023-01-31 21:03:08 +0100

    Add BookDto

---
+++ WebClient/DTO/BookDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebClient.DTO;
+
+public class Book
+{
+    [Required]
+    public int Id { get; set; }
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public string Author { get; set; }
+    [Required]
+    public int Year { get; set; }
+    [Required]
+    public string Country { get; set; }
+    [Required]
+    public string Language { get; set; }
+    [Required]
+    public string ImageLink { get; set; }
+    [Required]
+    public int Stock { get; set; }
+}
