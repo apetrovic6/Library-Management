a03e354277d4ba1055b20850977f1c1dc7902d5a
apetrovic6 <petrovicante6@gmail.com>
2023-01-31 21:04:46 +0100

    Generic Service Interface

---
+++ WebClient/Services/Interfaces/IGenericService.cs
@@ -0,0 +1,13 @@
+﻿using StrawberryShake;
+using WebClient.DTO;
+
+namespace WebClient.Services.Interfaces;
+
+public interface IGenericService<T> 
+{
+    Task<List<T>> GetAll();
+    Task<T> GetById(int id);
+    Task<(T, IReadOnlyList<IClientError>, bool IsSuccess)> Create<K>(K createDto);
+    Task<T> Update(int id,  BaseDto dto);
+    Task<T> Delete(int id);
+}
