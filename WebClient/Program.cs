6d213f421ff282258af78cdfee7cb9b415bbf58c
apetrovic6 <petrovicante6@gmail.com>
2023-01-31 21:07:14 +0100

    Setup DI for BookService

--- WebClient/Program.cs
+++ WebClient/Program.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using WebClient;
+using WebClient.DTO;
+using WebClient.Services;
+using WebClient.Services.Interfaces;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,6 +13,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.AddMudServices();
+builder.Services.AddScoped<IGenericService<Book>, BookService>();
+
 builder.Services.AddBooksClient()
     .ConfigureHttpClient(client => client.BaseAddress = new Uri("https://acme.com/graphql"));
 
