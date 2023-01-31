7dce93a7ed63087ff93e995ec4fb70c97ac8f8dd
apetrovic6 <petrovicante6@gmail.com>
2023-01-31 21:08:49 +0100

    Refactor BooksBase class

--- WebClient/Pages/Books/BooksBase.razor.cs
+++ WebClient/Pages/Books/BooksBase.razor.cs
@@ -1,19 +1,20 @@
 ﻿using BooksGQL;
 using Microsoft.AspNetCore.Components;
+using WebClient.DTO;
+using WebClient.Services.Interfaces;
 
 namespace WebClient.Pages.Books;
 
 public class BooksBase : ComponentBase
 {
-    [Inject] private BooksClient _client { get; set; }
+    [Inject] private IGenericService<Book> _bookService { get; set; }
     [Inject] private NavigationManager Navigation { get; set; }
-    protected IGetBooksResult? FetchedBooks { get; set; }
+    protected List<Book> FetchedBooks { get; set; } = new();
 
     protected override async Task OnInitializedAsync()
     {
-        var result = await _client.GetBooks.ExecuteAsync();
-
-        FetchedBooks = result.Data;
+        var result = await _bookService.GetAll();
+        FetchedBooks = result;
     }
 
     protected void NavigateToBook(int Id)
