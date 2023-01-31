1b40aa198cd7c7b51d3ce372c8ca05bb1c58df6a
apetrovic6 <petrovicante6@gmail.com>
2023-01-31 17:19:10 +0100

    AddBook DTO for Blazor validation

---
+++ WebClient/DTO/AddBookDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebClient.DTO;
+
+public class AddBookDto
+{
+    [Required] 
+    public string Title { get; set; }
+
+    [Required] 
+    public string Author { get; set; }
+
+    [Required] 
+    public int Year { get; set; }
+
+    [Required] 
+    public string Country { get; set; }
+
+    [Required] 
+    public string Language { get; set; }
+
+    [Required] 
+    public string ImageLink { get; set; }
+
+    [Required, Range(0, int.MaxValue, ErrorMessage = "Can't be a negative number")]
+    public int Stock { get; set; }
+}
+
