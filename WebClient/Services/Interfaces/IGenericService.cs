3c5d48f98734dd6091d44db6523360c67d693c7c
apetrovic6 <petrovicante6@gmail.com>
2023-02-01 12:36:23 +0100

    Redefine Generic Service Interface

--- WebClient/Services/Interfaces/IGenericService.cs
+++ WebClient/Services/Interfaces/IGenericService.cs
@@ -8,6 +8,6 @@ public interface IGenericService<T>
     Task<List<T>> GetAll();
     Task<T> GetById(int id);
     Task<(T, IReadOnlyList<IClientError>, bool IsSuccess)> Create<K>(K createDto);
-    Task<T> Update<K>(int id,  K updateDto);
-    Task<T> Delete(int id);
+    Task<(T, IReadOnlyList<IClientError>, bool IsSuccess)> Update<K>(int id,  K updateDto);
+    Task<(bool,IReadOnlyList<IClientError>)> Delete(int id);
 }
