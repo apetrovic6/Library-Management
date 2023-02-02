a593f2354b86ddf550ca4d280ee1627c9de3307c
apetrovic6 <petrovicante6@gmail.com>
2023-02-02 12:52:23 +0100

    Set up the Authors DB Context

---
+++ Authors/Data/AuthorsDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Authors.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authors.Data;
+
+public class AuthorsDbContext : DbContext
+{
+    public AuthorsDbContext(DbContextOptions<AuthorsDbContext> options) : base(options) {}
+    
+    public DbSet<Author> Authors { get; set; }
+}
