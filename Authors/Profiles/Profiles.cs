8b69c83cd0442232b40efb561412d21c762e49ad
apetrovic6 <petrovicante6@gmail.com>
2023-02-02 16:05:51 +0100

    Setup AutoMapper profiles for Authors

---
+++ Authors/Profiles/Profiles.cs
@@ -0,0 +1,30 @@
+﻿using Authors.Models;
+using AutoMapper;
+
+namespace Authors.Profiles;
+
+public class Profiles : Profile
+{
+    public Profiles()
+    {
+        CreateMap<List<AuthorEntity>, GetAuthorsResponse>()
+            .ForMember(dest => dest.Authors,
+                src => src.MapFrom(
+                    x => x
+                )
+            );
+        CreateMap<AuthorEntity, AuthorDTO>();
+        CreateMap<AuthorDTO, AuthorEntity>();
+        CreateMap<AuthorEntity, GetAuthorByIdResponse>()
+            .ForMember(dest => dest.Author,
+                src => src.MapFrom(
+                    x => x
+                ));
+        CreateMap<CreateAuthorRequest, AuthorEntity>();
+        CreateMap<AuthorEntity, CreateAuthorResponse>();
+        CreateMap<AuthorEntity, UpdateAuthorResponse>()
+            .ForMember(dest => dest.Author,
+                src => src.MapFrom(
+                    x => x));
+    }
+}
