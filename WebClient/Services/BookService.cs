faa21df974aa78f04b67e44514b6ab60c2e9971f
apetrovic6 <petrovicante6@gmail.com>
2023-02-06 11:14:05 +0100

    Auto renaming stuff

--- WebClient/Services/BookService.cs
+++ WebClient/Services/BookService.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
-using Books.Models;
-using BooksGQL;
+using LibraryGQL;
 using MudBlazor.Extensions;
 using StrawberryShake;
 using WebClient.DTO;
@@ -8,53 +7,53 @@ using WebClient.Services.Interfaces;
 
 namespace WebClient.Services;
 
-public class BookService : IGenericService<Book>
+public class BookService : IGenericService<BookDto>
 {
-    private readonly BooksClient _client;
+    private readonly LibraryClient _client;
     private readonly IMapper _mapper;
 
-    public BookService(BooksClient client, IMapper mapper)
+    public BookService(LibraryClient client, IMapper mapper)
     {
         _client = client;
         _mapper = mapper;
     }
-    public async Task<(PagedResult<Book>, IReadOnlyList<IClientError>, bool IsSuccess)> GetAll<PagingInput, Filterinput>(PagingInput pagingInput,Filterinput filterInput)
+    public async Task<(PagedResult<BookDto>, IReadOnlyList<IClientError>, bool IsSuccess)> GetAll<PagingInput, Filterinput>(PagingInput pagingInput,Filterinput filterInput)
     {
-        var input = pagingInput.As<BooksGQL.PagingInput>();
+        var input = pagingInput.As<LibraryGQL.PagingInput>();
         var filter = filterInput.As<BookFilterInput>();
         var res = await _client.GetBooks.ExecuteAsync(input, filter);
-        var a = res.Data.Books.Data;
-        var bookList = _mapper.Map<IReadOnlyList<IGetBooks_Books_Data>, List<Book>>(a);
-        var pagedResult = new PagedResult<Book>
+        var data = res.Data.Books.Data;
+        var bookList = _mapper.Map<IReadOnlyList<IGetBooks_Books_Data>, List<BookDto>>(data);
+        var pagedResult = new PagedResult<BookDto>
             { Data = bookList, PageInfo = new PageInfo { Total = res.Data.Books.PageInfo.Total } };
 
         return (pagedResult, res.Errors, res.IsSuccessResult());
     }
 
-    public async Task<Book> GetById(int id)
+    public async Task<BookDto> GetById(int id)
     {
         var res = await _client.GetBookById.ExecuteAsync(id);
-        return _mapper.Map<Book>(res.Data.BookById.Book);
+        return _mapper.Map<BookDto>(res.Data.BookById.Book);
     }
     
 
-    public async Task<(Book, IReadOnlyList<IClientError>, bool IsSuccess)> Create<K>(K createDto)
+    public async Task<(BookDto, IReadOnlyList<IClientError>, bool IsSuccess)> Create<K>(K createDto)
     {
         var createBookInput = _mapper.Map<CreateBookInput>(createDto);
         var res = await _client.CreateBook.ExecuteAsync(createBookInput);
-        var createdBook = _mapper.Map<Book>(res.Data.CreateBook.Book);
+        var createdBook = _mapper.Map<BookDto>(res.Data.CreateBook.Book);
 
         return (createdBook, res.Errors, res.IsSuccessResult());
     }
 
-    public async Task<(Book, IReadOnlyList<IClientError>, bool IsSuccess)> Update<K>(int id, K updateDto)
+    public async Task<(BookDto, IReadOnlyList<IClientError>, bool IsSuccess)> Update<K>(int id, K updateDto)
     {
         try
         {
             var updateInput = _mapper.Map<UpdateBookInput>(updateDto);
 
             var res = await _client.UpdateBook.ExecuteAsync(id, updateInput);
-            var updatedBook = _mapper.Map<IUpdateBook_UpdateBook_Book, Book>(res.Data.UpdateBook.Book);
+            var updatedBook = _mapper.Map<IUpdateBook_UpdateBook_Book, BookDto>(res.Data.UpdateBook.Book);
             var a = res.Data.UpdateBook.Book;
             return (updatedBook, res.Errors, res.IsSuccessResult());
         }
